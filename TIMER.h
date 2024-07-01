5c5e6a96a590573321dd0afadb5c4200280044ea
Lean Gobbi <115960321+LeanGobbi@users.noreply.github.com>
2024-07-01 11:43:44 -0300

    Update TIMER.h

--- TIMER.h
+++ TIMER.h
@@ -1,6 +1,8 @@
 #ifndef TIMER_H
 #define TIMER_H
-
+#define T 2
+#define PREESCALER 1024
+#define GET_OCR1A ((F_CPU*T/PREESCALER)-1)  //Formula para el OCR1A
 #include <avr/io.h>
 #include "lib_delay.h"
 /*-----------Prototipo funciones publicas-----------*/
