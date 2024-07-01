7875761ccc26d27b0679a2329fce685e9160981a
Lean Gobbi <115960321+LeanGobbi@users.noreply.github.com>
2024-06-30 21:15:16 -0300

    Update TIMER.c

--- TIMER.c
+++ TIMER.c
@@ -1,22 +1,20 @@
 #include "TIMER.h"
 #define T 2
 #define PREESCALER 1024
-//Al final si se puede hacer de 2 segundos jaja -> T = 2 y PREESCALER = 1024 --> OCR1A = 31249 que si entra en 16 bits.
-
 #define GET_OCR1A ((F_CPU*T/PREESCALER)-1)  //Formula para el OCR1A
 
 void TIMER_Init() {		  //Configuracion del Timer de 2s
 	TCCR1B |= (1 << WGM12);               // Configura el modo CTC (Clear Timer on Compare Match)
 	TCCR1B |= ((1 << CS12) | (1 << CS10));// Configura el preescalador a 1024
-	OCR1A = GET_OCR1A;                    // Establece el valor de comparaci髇
+	OCR1A = GET_OCR1A;                    // Establece el valor de comparaci贸n
 	TIMER_Enable();
 }
 
 void TIMER_Enable(){
-	TIMSK1 |= (1 << OCIE1A);            // Habilita la interrupci髇 de comparaci髇 A	
+	TIMSK1 |= (1 << OCIE1A);            // Habilita la interrupci贸n de comparaci贸n A	
 	TCNT1 = 0;							//Reiniciar contador
 }
 
 void TIMER_Disable(){
-	TIMSK1 &= ~(1 << OCIE1A);              // Deshabilita la interrupci髇 de comparaci髇 A
+	TIMSK1 &= ~(1 << OCIE1A);              // Deshabilita la interrupci贸n de comparaci贸n A
 }
