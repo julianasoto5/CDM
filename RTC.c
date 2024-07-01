8f6347d70800499921dbddecedad097c4061a40b
Lean Gobbi <115960321+LeanGobbi@users.noreply.github.com>
2024-06-30 21:10:51 -0300

    Update RTC.c

--- RTC.c
+++ RTC.c
@@ -11,28 +11,14 @@ uint8_t bcdToDec(uint8_t val) {
     return ( (val/16*10) + (val%16) );
 }
 
-uint8_t RTC_isRtcRunning(void) {
-    uint8_t status;
-    I2C_Start();
-    I2C_Write((DS3231_I2C_ADDRESS << 1) | 0); // Dirección de escritura
-    I2C_Write(0x0F); // Dirección del registro de estado
-    I2C_Stop();
-    I2C_Start();
-    I2C_Write((DS3231_I2C_ADDRESS << 1) | 1); // Dirección de lectura
-    status = I2C_Read_nack();
-    I2C_Stop();
-
-    return !(status & 0x80); // Si el bit OSF (Oscillator Stop Flag) está bajo, el RTC está corriendo
-}
-
 void RTC_setDateTime(uint8_t year, uint8_t month, uint8_t day, uint8_t hour, uint8_t minute, uint8_t second) {
     I2C_Start();
-    I2C_Write((DS3231_I2C_ADDRESS << 1) | 0); // Dirección de escritura
-    I2C_Write(0); // Comienza en la dirección 0x00
+    I2C_Write((DS3231_I2C_ADDRESS << 1) | 0); // DirecciÃ³n de escritura
+    I2C_Write(0); // Comienza en la direcciÃ³n 0x00
     I2C_Write(decToBcd(second));
     I2C_Write(decToBcd(minute));
     I2C_Write(decToBcd(hour));
-    I2C_Write(0); // Día de la semana (no se usa)
+    I2C_Write(0); // DÃ­a de la semana (no se usa)
     I2C_Write(decToBcd(day));
     I2C_Write(decToBcd(month));
     I2C_Write(decToBcd(year));
@@ -41,15 +27,15 @@ void RTC_setDateTime(uint8_t year, uint8_t month, uint8_t day, uint8_t hour, uin
 
 void RTC_getDateTime(uint8_t *year, uint8_t *month, uint8_t *day, uint8_t *hour, uint8_t *minute, uint8_t *second) {
     I2C_Start();
-    I2C_Write((DS3231_I2C_ADDRESS << 1) | 0); // Dirección de escritura
-    I2C_Write(0); // Comienza en la dirección 0x00
+    I2C_Write((DS3231_I2C_ADDRESS << 1) | 0); // DirecciÃ³n de escritura
+    I2C_Write(0); // Comienza en la direcciÃ³n 0x00
     I2C_Stop();
     I2C_Start();
-    I2C_Write((DS3231_I2C_ADDRESS << 1) | 1); // Dirección de lectura
+    I2C_Write((DS3231_I2C_ADDRESS << 1) | 1); // DirecciÃ³n de lectura
     *second = bcdToDec(I2C_Read_ack());
     *minute = bcdToDec(I2C_Read_ack());
     *hour = bcdToDec(I2C_Read_ack());
-    I2C_Read_ack(); // Día de la semana (no se usa)
+    I2C_Read_ack(); // DÃ­a de la semana (no se usa)
     *day = bcdToDec(I2C_Read_ack());
     *month = bcdToDec(I2C_Read_ack());
     *year = bcdToDec(I2C_Read_nack());
@@ -57,8 +43,6 @@ void RTC_getDateTime(uint8_t *year, uint8_t *month, uint8_t *day, uint8_t *hour,
 }
 
 void RTC_setup() {
-    // Verificar si el RTC está corriendo
-        // Configurar la fecha y hora iniciales
-        RTC_setDateTime(24, 6, 24, 2, 24, 0); // Configuración inicial
+        RTC_setDateTime(24, 6, 24, 2, 24, 0); // ConfiguraciÃ³n inicial
     
 }
