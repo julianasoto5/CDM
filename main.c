14397eb7238f82d36b74c80c9cd55d33f141fa8f
Lean Gobbi <115960321+LeanGobbi@users.noreply.github.com>
2024-06-30 21:20:52 -0300

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main(void) {
     UART_Init(0x67); // baud rate calculado para 9600 bps
     DHT11_Start();
     I2C_Init();
-    //RTC_setup();
+    RTC_setup();
 	TIMER_Init();
 	sei();
     while (1) {
